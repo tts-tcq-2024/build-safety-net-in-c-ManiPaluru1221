67d53d8f46a6f428e1b562cf5e499ca6290358aa
ManiPaluru1221 <palurumani1999@gmail.com>
2024-07-09 10:07:54 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,76 +6,26 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
  // ASSERT_EQ(soundex,"A200");
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("A", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
-  char soundex[5];
-  generateSoundex("F", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("AEIOU", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("AEIOUY", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("BCDFG]HJKLMNPQRSTVWXYZY", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("Robert", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("Rupert", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("Tymczak", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("RoBert", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("AehioU", soundex);
- 
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- 
   char soundex[5];
   generateSoundex("Aehea", soundex);
- 
 }
