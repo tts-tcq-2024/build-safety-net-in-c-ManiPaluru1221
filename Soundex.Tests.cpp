3d44e4bf8a6387c9cf6b0c75047acb73a0872fdd
ManiPaluru1221 <palurumani1999@gmail.com>
2024-07-09 10:10:55 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -6,26 +6,23 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
  // ASSERT_EQ(soundex,"A200");
-  char soundex[5];
-  generateSoundex("", soundex);
-  char soundex[5];
   generateSoundex("A", soundex);
-  char soundex[5];
+
   generateSoundex("AEIOU", soundex);
-  char soundex[5];
+
   generateSoundex("AEIOUY", soundex);
-  char soundex[5];
+
   generateSoundex("BCDFG]HJKLMNPQRSTVWXYZY", soundex);
-  char soundex[5];
+
   generateSoundex("Robert", soundex);
-  char soundex[5];
+
   generateSoundex("Rupert", soundex);
-  char soundex[5];
+ 
   generateSoundex("Tymczak", soundex);
-  char soundex[5];
+
   generateSoundex("RoBert", soundex);
-  char soundex[5];
+  
   generateSoundex("AehioU", soundex);
-  char soundex[5];
+
   generateSoundex("Aehea", soundex);
 }
